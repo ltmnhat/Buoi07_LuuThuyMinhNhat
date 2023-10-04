2de1c80ab5355254cb2e77e723299bd0ef3498c4
Luu Thuy Minh Nhat <minhnhat240820032019@gmail.com>
2023-10-04 14:29:18 +0700

    add Bai 1 Buoi 7

---
+++ Buoi07_LuuThuyMinhNhat/Bai1_MinhNhat/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Bai1_MinhNhat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnNhap = new System.Windows.Forms.Button();
+            this.btnTiepTuc = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNhapSo = new System.Windows.Forms.TextBox();
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.txtTinhTong = new System.Windows.Forms.TextBox();
+            this.txtTongChan = new System.Windows.Forms.TextBox();
+            this.txtTongLe = new System.Windows.Forms.TextBox();
+            this.btnTinhTong = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập số";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dãy vừa nhập";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tổng các phần tử trong dãy";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tổng Chẵn";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(164, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tổng Lẻ";
+            // 
+            // btnNhap
+            // 
+            this.btnNhap.Location = new System.Drawing.Point(201, 62);
+            this.btnNhap.Name = "btnNhap";
+            this.btnNhap.Size = new System.Drawing.Size(75, 23);
+            this.btnNhap.TabIndex = 6;
+            this.btnNhap.Text = "Nhập";
+            this.btnNhap.UseVisualStyleBackColor = true;
+            this.btnNhap.Click += new System.EventHandler(this.btnNhap_Click);
+            // 
+            // btnTiepTuc
+            // 
+            this.btnTiepTuc.Location = new System.Drawing.Point(25, 214);
+            this.btnTiepTuc.Name = "btnTiepTuc";
+            this.btnTiepTuc.Size = new System.Drawing.Size(75, 23);
+            this.btnTiepTuc.TabIndex = 7;
+            this.btnTiepTuc.Text = "Tiếp Tục";
+            this.btnTiepTuc.UseVisualStyleBackColor = true;
+            this.btnTiepTuc.Click += new System.EventHandler(this.btnTiepTuc_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(201, 214);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 8;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(45, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(221, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Nhập Dãy Số và Tính Tổng";
+            // 
+            // txtNhapSo
+            // 
+            this.txtNhapSo.Location = new System.Drawing.Point(75, 64);
+            this.txtNhapSo.Name = "txtNhapSo";
+            this.txtNhapSo.Size = new System.Drawing.Size(103, 20);
+            this.txtNhapSo.TabIndex = 10;
+            // 
+            // txtIn
+            // 
+            this.txtIn.Location = new System.Drawing.Point(103, 95);
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(173, 20);
+            this.txtIn.TabIndex = 11;
+            // 
+            // txtTinhTong
+            // 
+            this.txtTinhTong.Location = new System.Drawing.Point(167, 120);
+            this.txtTinhTong.Name = "txtTinhTong";
+            this.txtTinhTong.Size = new System.Drawing.Size(64, 20);
+            this.txtTinhTong.TabIndex = 12;
+            // 
+            // txtTongChan
+            // 
+            this.txtTongChan.Location = new System.Drawing.Point(89, 156);
+            this.txtTongChan.Name = "txtTongChan";
+            this.txtTongChan.Size = new System.Drawing.Size(53, 20);
+            this.txtTongChan.TabIndex = 13;
+            // 
+            // txtTongLe
+            // 
+            this.txtTongLe.Location = new System.Drawing.Point(217, 156);
+            this.txtTongLe.Name = "txtTongLe";
+            this.txtTongLe.Size = new System.Drawing.Size(59, 20);
+            this.txtTongLe.TabIndex = 14;
+            // 
+            // btnTinhTong
+            // 
+            this.btnTinhTong.Location = new System.Drawing.Point(112, 214);
+            this.btnTinhTong.Name = "btnTinhTong";
+            this.btnTinhTong.Size = new System.Drawing.Size(75, 23);
+            this.btnTinhTong.TabIndex = 15;
+            this.btnTinhTong.Text = "Tính Tổng";
+            this.btnTinhTong.UseVisualStyleBackColor = true;
+            this.btnTinhTong.Click += new System.EventHandler(this.btnTinhTong_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 293);
+            this.Controls.Add(this.btnTinhTong);
+            this.Controls.Add(this.txtTongLe);
+            this.Controls.Add(this.txtTongChan);
+            this.Controls.Add(this.txtTinhTong);
+            this.Controls.Add(this.txtIn);
+            this.Controls.Add(this.txtNhapSo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnTiepTuc);
+            this.Controls.Add(this.btnNhap);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnNhap;
+        private System.Windows.Forms.Button btnTiepTuc;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNhapSo;
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.TextBox txtTinhTong;
+        private System.Windows.Forms.TextBox txtTongChan;
+        private System.Windows.Forms.TextBox txtTongLe;
+        private System.Windows.Forms.Button btnTinhTong;
+    }
+}
+
---
+++ Buoi07_LuuThuyMinhNhat/Bai1_MinhNhat/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai1_MinhNhat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool check(int a)
+        {
+            if (a % 2 == 0)
+                return true;
+            return false;
+        }
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            string input = txtNhapSo.Text;
+            string[] DaySo = input.Split(' ');
+            int[] MangSoNguyen = Array.ConvertAll(DaySo, int.Parse);
+            txtIn.Text = string.Join(" ", MangSoNguyen);
+
+        }
+
+        private void btnTiepTuc_Click(object sender, EventArgs e)
+        {
+            txtNhapSo.Clear();
+            txtIn.Clear();
+            txtTinhTong.Clear();
+            txtTongChan.Clear();
+            txtTongLe.Clear();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnTinhTong_Click(object sender, EventArgs e)
+        {
+            // Tinh tong cac so
+            string input = txtIn.Text;
+            string[] DaySo = input.Split(' ');
+            int[] MangSoNguyen = Array.ConvertAll(DaySo, int.Parse);
+            txtTinhTong.Text = MangSoNguyen.Sum().ToString();
+            int TongChan=0;
+            int TongLe = 0;
+            for(int i=0;i<MangSoNguyen.Count();i++)
+            {
+                //Tong chan
+                if (check(MangSoNguyen[i]))
+                {
+                    TongChan=TongChan + MangSoNguyen[i];
+                    txtTongChan.Text = TongChan.ToString();
+                }
+                else
+                {
+                    //Tong le
+                    TongLe = TongLe + MangSoNguyen[i];
+                    txtTongLe.Text = TongLe.ToString();
+                }
+            }
+
+        }
+    }
+}
